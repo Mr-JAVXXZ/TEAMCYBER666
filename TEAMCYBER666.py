9c6a06ae35deed63f17cb58e0e0dfdf92ccbead5
Mr-JAVXXZ <150129084+Mr-JAVXXZ@users.noreply.github.com>
2023-11-07 16:53:14 +0700

    Create TEAMCYBER666.py

---
+++ TEAMCYBER666.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import time
+import random
+import socket
+
+##################
+#BANTU SUPPORT AKUN SAYA YA!!
+##################
+os.system("clear")
+os.system("figlet YOUR CERTIFICATE")
+print 
+print "==============PROFIL ACCOUNT TERMUX YOU=============="
+print "YOUR  NAME     = JAVA CHRISAND HECKA ANGGARA / ID SERVER"
+print "FROM THE GROUP = TEAM CYBER INTERNASIONAL OFFICIAL"
+print "COLLABORATION  = BCNRI BADAN CYBER NEGARA REPUBLIK INDONESIA"
+print "========BADAN CYBER NEGARA REPUBLIK INDONESIA========"
+exit =raw_input("CLICK YES FOR EXIT PROFILE: ")
+os.system("clear")
